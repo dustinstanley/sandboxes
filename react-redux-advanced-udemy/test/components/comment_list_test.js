cc510efc04d81160e9f96ee451e4cd2fdc44daf0
Dustin Stanley <dustin.stanley@gmail.com>
2018-12-08 01:08:55 -0800

    Remove old react-redux-advanced-udemy class folder

--- react-redux-advanced-udemy/test/components/comment_list_test.js
+++
@@ -1,9 +0,0 @@
-import { renderComponent, expect} from '../test_helper';
-import CommentList from '../../src/components/comment_list';
-
-describe('CommentList', () => {
-
-  it('', () => {
-
-  });
-});
