bc6105b914dbb1ac7444f86542482ab8bfbcc67f
Dustin Stanley <dustin.stanley@gmail.com>
2019-05-21 21:50:59 -0700

    Add rust sandbox

---
+++ rust-sandbox/tests/test.rs
@@ -0,0 +1,13 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let ints = [1, 2, 3, 4, 5];
+        let slice1 = &ints[0..2];
+        let slice2 = &ints[1..];  // open range!
+
+        println!("ints {:?}", ints);
+        println!("slice1 {:?}", slice1);
+        println!("slice2 {:?}", slice2);
+    }
+}
